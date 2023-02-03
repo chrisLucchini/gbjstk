cd86cbc6229d80258fbe6524e71b82e3d073db4d
Christophe Lucchini <christophelucchini@hotmail.com>
2023-02-03 11:44:12 +0100

    Fix issue related to click event listener outside iFrame context

--- GBJSTK.js
+++ GBJSTK.js
@@ -327,20 +327,23 @@ var gb = (function() {
 			gbAngularMode = true;
 			gbDevMode = false;
 
-			// Intercept clicks on links in order to call the corresponding method
-			window.addEventListener("click", function(evt) {
-				const target = evt.target.closest("a");
-				if (target) {
-					const href = target.getAttribute("href") || "";
-					const isAnchor = href.startsWith("#");
-					const isJS = target.protocol.startsWith("javascript");
-					if (!isAnchor && !isJS) {
-						evt.preventDefault();
-						parent.postMessage({url: href}, "*");
-						return false;
+			// Test if context is iFrame
+			if (window.self !== window.top) {
+				// Intercept clicks on links in order to call the corresponding method
+				window.addEventListener("click", function(evt) {
+					const target = evt.target.closest("a");
+					if (target) {
+						const href = target.getAttribute("href") || "";
+						const isAnchor = href.startsWith("#");
+						const isJS = target.protocol.startsWith("javascript");
+						if (!isAnchor && !isJS) {
+							evt.preventDefault();
+							parent.postMessage({url: href}, "*");
+							return false;
+						}
 					}
-				}
-			});
+				});
+			}
 		}
 
 		/* Function : gbWebsiteSetData
